28a748769fd2bbf1f95be16837adad13f82c26c5
xo <xo@3013f14e-8ced-48c3-93c5-da350a8f9142>
2008-02-25 10:50:51 +0000

    Added licensing, bumped to 0.6.

--- README.txt
+++ README.txt
@@ -1,5 +1,11 @@
-dir2cast by Ben XO v0.5 (2008-02-25)
-====================================
+dir2cast by Ben XO v0.6 (2008-02-25)
+================================================================================
+
+http://www.ben-xo.com/dir2cast
+
+
+INTRODUCTION
+================================================================================
 
 Hello!
 
@@ -33,11 +39,64 @@ Features:
 
 * Works from the command line, if you want.
 
--- 
-Ben
+REQUIREMENTS
+================================================================================
 
-HISTORY:
+dir2cast requires PHP 5.1.
 
+dir2cast makes use of getID3 by James Heinrich, Allan Hansen, although it does
+not require the whole thing. A cut down version of getID3 is supplied at
+http://www.ben-xo.com/dir2cast. You will need to download this and install it
+with dir2cast.php. (It is not bundled in the same file for licensing reasons.
+The full version of getID3 is available at http://getid3.sourceforge.net/
+
+
+INSTALLATION
+================================================================================
+
+Step 1. Upload dir2cast.php to the web server.
+Step 2. Upload getID3 to a folder called 'getID3'. You can download getID3 from
+        the same place as dir2cast.
+
+
+COPYRIGHT
+================================================================================
+
+Copyright (c) 2008, Ben XO (me@ben-xo.com).
+
+The software is released under the BSD License.
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, 
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, 
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of the <ORGANIZATION> nor the names of its contributors
+      may be used to endorse or promote products derived from this software 
+      without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+HISTORY
+================================================================================
+
+0.6 2008-02-25 Updated the licensing information.
 0.5 2008-02-25 Fixed losing itunes item tags when a dir changes.
 0.4 2008-02-24 Added a whole error handler with pretty errors
 0.3 2008-02-24 Fixed the auto URL detection code. Made the config clearer.
--- dir2cast.php
+++ dir2cast.php
@@ -1,8 +1,35 @@
 <?php
 
-/*
- * Copyright 2008 Ben XO (me@ben-xo.com).
+/******************************************************************************
+ * Copyright (c) 2008, Ben XO (me@ben-xo.com).
+ *
+ * All rights reserved.
  * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of dir2cast nor the names of its contributors may be used 
+ *    to endorse or promote products derived from this software without specific 
+ *    prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ ******************************************************************************
+ *
  * USAGE:
  * 
  * After editing the settings below, visit:
@@ -161,8 +188,10 @@ $itunes_categories = array(
 /* DEFAULTS *********************************************/
 
 // error handler needs these, so let's set them now.
-define('VERSION', '0.5');
-define('GENERATOR', 'dir2cast ' . VERSION . ' by Ben XO (http://www.ben-xo.com/dir2cast/)');
+define('VERSION', '0.6');
+define('DIR2CAST_HOMEPAGE', 'http://www.ben-xo.com/dir2cast/');
+define('GENERATOR', 'dir2cast ' . VERSION . ' by Ben XO (' . DIR2CAST_HOMEPAGE . ')');
+
 
 error_reporting(E_ALL);
 set_error_handler( array('ErrorHandler', 'handle_error') );
@@ -288,7 +317,13 @@ if(!defined('ITUNES_OWNER_EMAIL'))
 
 /* EXTENALS *********************************************/
 
-require_once('getID3/getid3.php');
+if(file_exists('getID3/getid3.php'))
+	include_once('getID3/getid3.php');
+elseif(file_exists('getid3/getid3.php'))
+	include_once('getid3/getid3.php');
+else
+	throw new Exception('dir2cast requires getID3. You should download this from <a href="' . DIR2CAST_HOMEPAGE . '">' . DIR2CAST_HOMEPAGE .'</a> and install it with dir2cast.');
+
 
 /* CLASSES **********************************************/
 
@@ -443,7 +478,8 @@ class iTunes_Podcast_Helper extends GetterSetter implements Podcast_Helper {
 		 * 	<itunes:author>John Doe</itunes:author>
 		 *	<itunes:duration>7:04</itunes:duration>
 		 *	<itunes:subtitle>A short primer on table spices</itunes:subtitle>
-		 *	<itunes:summary>This week we talk about salt and pepper shakers, comparing and contrasting pour rates, construction materials, and overall aesthetics. Come and join the party!</itunes:summary>
+		 *	<itunes:summary>This week we talk about salt and pepper shakers, 
+		 *                  [...] Come and join the party!</itunes:summary>
 		 *	<itunes:keywords>salt, pepper, shaker, exciting</itunes:keywords>
 		 */
 
