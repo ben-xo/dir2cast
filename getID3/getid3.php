6832a50e0475cc128b19d80655f091ebe5f1d94d
xo <xo@3013f14e-8ced-48c3-93c5-da350a8f9142>
2010-07-31 21:06:27 +0000

    eregi -> preg_match in getID3.

--- getID3/getid3.php
+++ getID3/getid3.php
@@ -109,7 +109,7 @@ class getid3
 
         // Check memory limit.
         $memory_limit = ini_get('memory_limit');
-        if (eregi('([0-9]+)M', $memory_limit, $matches)) {
+        if (preg_match('/([0-9]+)M/i', $memory_limit, $matches)) {
             // could be stored as "16M" rather than 16777216 for example
             $memory_limit = $matches[1] * 1048576;
         }
