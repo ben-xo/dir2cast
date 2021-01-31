b140ed3e746e7e658af1a978c69079d32cd24f92
Ben Isaacs <ben@last.fm>
2021-01-31 23:52:28 +0000

    Add more test: cached output is valid

--- test/DefaultsTest.php
+++ test/DefaultsTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 final class DefaultsTest extends TestCase
 {
-    public $tmpdir;
+    public $file = 'out.xml';
 
     public function setUp(): void
     {
@@ -19,11 +19,31 @@ final class DefaultsTest extends TestCase
         $output = array();
         $returncode = false;
         exec('php dir2cast.php --output=out.xml', $output, $returncode);
+        $this->assertTrue(file_exists($this->file));
+
+        $content = file_get_contents($this->file);
+        $this->assertTrue(strlen($content) > 0);
+
+        // warns the podcast is empty
         $this->assertSame(
             'Writing RSS to: out.xml\n** Warning: generated podcast found no episodes.',
             implode('\n', $output)
         );
         $this->assertSame(255, $returncode);
+
+        // caches the output in the default temp folder
+        $this->assertTrue(is_dir('./temp'));
+        $cached_output_files = glob('./temp/*.xml');
+        $this->assertSame(1, sizeof($cached_output_files));
+
+        // caches what was generated
+        $this->assertSame(
+            $content,
+            file_get_contents($cached_output_files[0])
+        );
+
+        // generated valid XML
+        $this->assertTrue(is_valid_xml($this->file));
     }
 
     public function tearDown(): void
