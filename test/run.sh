1049a4c16c3812236d11cfa7745ab89929f08277
Ben Isaacs <ben@last.fm>
2021-02-07 16:57:00 +0000

    Make it easier to run specific tests by passing them as options to run.sh

--- test/run.sh
+++ test/run.sh
@@ -5,4 +5,9 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd "$SCRIPT_DIR"
 
-phpunit --bootstrap bootstrap.php .
+tests=($@)
+if [[ "$tests" == "" ]]; then
+	tests=('.')
+fi
+
+phpunit --bootstrap bootstrap.php $tests
