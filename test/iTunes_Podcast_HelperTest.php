eb8a8692fbdec68d8a8c8e19486f13c4987c26f1
Ben Isaacs <ben@last.fm>
2021-02-10 23:39:17 +0000

    TODO for iTunes_Podcast_Helper appendToItem()

--- test/iTunes_Podcast_HelperTest.php
+++ test/iTunes_Podcast_HelperTest.php
@@ -159,33 +159,5 @@ final class iTunes_Podcast_HelperTest extends TestCase
 
     }
 
-    // public function test_adds_atom_link_to_channel_if_set()
-    // {
-    //     $mp = new MyPodcast();
-    //     $atom = $mp->addHelper(new Atom_Podcast_Helper());
-    //     $atom->setSelfLink('https://xo.am/dnb/bd/rss');
-    //     $content = $mp->generate();
-
-    //     $data = simplexml_load_string($content, 'SimpleXMLElement');
-    //     $link = $data->channel->children('http://www.w3.org/2005/Atom')->link;
-
-    //     $this->assertEquals('self', $link->attributes()->rel);
-    //     $this->assertEquals('https://xo.am/dnb/bd/rss', $link->attributes()->href);
-    //     $this->assertEquals('application/rss+xml', $link->attributes()->type);
-    // }
-
-    // public function test_adds_no_atom_link_to_channel_if_not_set()
-    // {
-    //     $mp = new MyPodcast();
-    //     $atom = $mp->addHelper(new Atom_Podcast_Helper());
-    //     $content = $mp->generate();
-
-    //     $data = simplexml_load_string($content, 'SimpleXMLElement');
-
-    //     $element_count = 0;
-    //     foreach ($data->channel->children('http://www.w3.org/2005/Atom') as $el) {
-    //         $element_count++;
-    //     }
-    //     $this->assertEquals(0, $element_count);
-    // }
+    // TODO: tests that assert that things are added to the individual RSS Items
 }
