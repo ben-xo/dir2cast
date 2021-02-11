16068669182d1be163805ba504149d7e1dd3af59
Ben Isaacs <ben@last.fm>
2021-02-11 22:48:49 +0000

    Add more test cases for RSS_File_Item

--- test/RSS_File_ItemTest.php
+++ test/RSS_File_ItemTest.php
@@ -20,4 +20,48 @@ final class RSS_File_ItemTest extends RSS_ItemTest
         return 'example.mp3';
     }
 
+    public function test_constructor_sets_default_properties_from_filename()
+    {
+        $item = $this->newRSSItem();
+        $this->assertEquals('example.mp3', $item->getFilename());
+        $this->assertEquals('example.mp3', $item->getTitle());
+        $this->assertEquals('http://www.example.com/mp3/example.mp3', $item->getLink());
+        $this->assertEquals('application/octet-stream', $item->getType());
+    }
+
+    public function test_filename_with_full_path()
+    {
+        $item = new RSS_File_Item(getcwd() . '/example.mp3');
+        $this->assertEquals(getcwd() . '/example.mp3', $item->getFilename());
+        $this->assertEquals('example.mp3', $item->getTitle());
+        $this->assertEquals('http://www.example.com/mp3/example.mp3', $item->getLink());
+    }
+
+    public function test_filename_without_extension()
+    {
+        $item = new RSS_File_Item('example');
+        $this->assertEquals('example', $item->getFilename());
+        $this->assertEquals('example', $item->getTitle());
+        $this->assertEquals('http://www.example.com/mp3/example', $item->getLink());
+    }
+
+    public function test_filename_with_full_path_without_extension()
+    {
+        $item = new RSS_File_Item(getcwd() . '/example');
+        $this->assertEquals(getcwd() . '/example', $item->getFilename());
+        $this->assertEquals('example', $item->getTitle());
+        $this->assertEquals('http://www.example.com/mp3/example', $item->getLink());
+    }
+
+    // test summary override
+    // test summary with matching filename
+    // test summary with matching filename with no extension
+
+
+    // test subtitle override
+    // test subtitle with matching filename
+    // test subtitle with matching filename with no extension
+
+    // test image png
+    // test image jpg
 }
