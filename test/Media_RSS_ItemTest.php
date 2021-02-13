c92068c0b9f0deff9e201ec8990229a44b21f4c3
Ben Isaacs <ben@last.fm>
2021-02-13 14:06:13 +0000

    Basic tests for file metadata based item attributes

--- test/Media_RSS_ItemTest.php
+++ test/Media_RSS_ItemTest.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 class Media_RSS_ItemTest extends RSS_File_ItemTest
 {
+    private $mtime;
 
     public static function setUpBeforeClass(): void
     {
@@ -15,8 +16,20 @@ class Media_RSS_ItemTest extends RSS_File_ItemTest
     public function newRSSItem()
     {
         // default tests are conducted with an empty file (which, therefore, has no ID3 tags to read)
-        file_put_contents('example.mp3', '');
+        file_put_contents('example.mp3', 'x');
+
+        // ensure that tests do not fail when getting unlucky with when the clock ticks.
+        $this->mtime = time();
+        touch('example.mp3', $this->mtime);
+
         return new Media_RSS_Item('example.mp3');
+    }
+
+    public function test_constructor_sets_default_properties_from_file_metadata()
+    {
+        $item = $this->newRSSItem();
+        $this->assertEquals('1', $item->getLength());
+        $this->assertEquals(date('r', $this->mtime), $item->getPubDate());
     }
 
     public function tearDown(): void
