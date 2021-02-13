1b3a0471e5d9c806cf9de1a35d8204ba3093a3e9
Ben Isaacs <ben@last.fm>
2021-02-13 18:20:04 +0000

    Fix the id3v2 versions of the LONG TITLE tests

--- test/Media_RSS_Item_id3v2_artist_title_LONG_TITLESTest.php
+++ test/Media_RSS_Item_id3v2_artist_title_LONG_TITLESTest.php
@@ -2,6 +2,21 @@
 
 use PHPUnit\Framework\TestCase;
 
-class Media_RSS_Item_id3v2_artist_title_LONG_TITLESTest extends Media_RSS_Item_id3v1_artist_title_LONG_TITLESTest
+class Media_RSS_Item_id3v2_artist_title_LONG_TITLESTest extends Media_RSS_Item_id3v2_artist_titleTest
 {
+    public static function setUpBeforeClass(): void
+    {
+        Media_RSS_Item_id3v2_artist_titleTest::setUpBeforeClass();
+        Media_RSS_Item::$LONG_TITLES = true;
+    }
+
+    public function getDefaultTitle()
+    {
+        return 'ARTIST6 - EXAMPLE6';
+    }
+
+    public function getDefaultSubtitle()
+    {
+        return '';
+    }
 }
