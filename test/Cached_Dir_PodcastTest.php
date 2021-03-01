2c65a128ad468f9b678ccf4013b35031d5fe9bf4
Ben Isaacs <ben@last.fm>
2021-03-01 20:42:39 +0000

    age dirs properly, don't use dates in future, assert buildDate is still valid when loading from cache

--- test/Cached_Dir_PodcastTest.php
+++ test/Cached_Dir_PodcastTest.php
@@ -27,6 +27,8 @@ class Cached_Dir_PodcastTest extends Dir_PodcastTest
     public function test_generate_saves_a_cache_file()
     {
         $this->createTestItems();
+        age_dir_by('.', 3600);
+
         $this->assertEmpty(glob('temp/*'));
         $mp = $this->newPodcast();
         $content = $mp->generate();
@@ -35,67 +37,101 @@ class Cached_Dir_PodcastTest extends Dir_PodcastTest
 
     public function test_uses_generated_cache_file_if_min_time_not_elapsed_yet()
     {
-        $filemtime = $this->createTestItems();
+        $this->createTestItems();
+        age_dir_by('.', 3600);
+
         $mp = $this->newPodcast();
         $content = $mp->generate();
         unset($mp); // release lock, in sub tests
 
+        age_dir_by('.', 2);
+
         // this should be ignored
         file_put_contents('extra.mp3', 'new data');
-        touch('extra.mp3', $filemtime + 200);
 
         $mp2 = $this->newPodcast();
         $content2 = $mp2->generate();
 
         // should not pick up extra.mp3 as the cache file isn't old enough
         $this->assertEquals($content, $content2);
+        $this->assertEquals(0, preg_match('/extra\.mp3/', $content2));
     }
 
     public function test_does_not_use_generated_cache_file_if_min_time_has_elapsed_and_theres_new_content()
     {
-        Cached_Dir_Podcast::$MIN_CACHE_TIME = -1;
-        $filemtime = $this->createTestItems();
+        $this->createTestItems();
+        age_dir_by('.', 3600);
+
         $mp = $this->newPodcast();
         $content = $mp->generate();
         unset($mp); // release lock, in sub tests
 
+        age_dir_by('.', 10);
+
         // this should be considered
         file_put_contents('extra.mp3', 'new data');
-        touch('extra.mp3', $filemtime + 200);
 
         $mp2 = $this->newPodcast();
         $content2 = $mp2->generate();
 
-        // should not pick up extra.mp3 as the cache file isn't old enough
+        // should pick up extra.mp3 as the cache file is older than the min, and there's new content
         $this->assertNotEquals($content, $content2);
+        $this->assertEquals(1, preg_match('/extra\.mp3/', $content2));
     }
 
 
     public function test_renews_cache_if_old_but_not_stale()
     {
-        $filemtime = $this->createTestItems();
-        touch('test1.mp3', $filemtime - 10); // older than min time
+        $this->createTestItems();
+        age_dir_by('.', 3600);
+
         $mp = $this->newPodcast();
         $content = $mp->generate();
         unset($mp); // release lock, in sub tests
 
-        // cache file now exists. Artificially age it by 6 seconds so it's older than MIN_CACHE_TIME
-        foreach(glob('temp/*.xml') as $filename)
-        {
-            touch($filename, $filemtime - 6);
-        }
+        age_dir_by('.', 3600);
 
         $mp2 = $this->newPodcast();
+
         $content2 = $mp2->generate();
 
         // should have used cache file anyway
         $this->assertEquals($content, $content2);
 
+        clearstatcache();
         foreach(glob('temp/*.xml') as $filename)
         {
             // cache file should have been refreshed
-            $this->assertGreaterThan($filemtime - 6, filemtime($filename));
+            $this->assertGreaterThan(time() - 1, filemtime($filename));
         }
+    }
+
+    public function test_lastBuildDate_is_valid_whether_served_from_cache_or_not()
+    {
+        $this->createTestItems();
+        age_dir_by('.', 3600);
+
+        $mp = $this->newPodcast();
+
+        $lastBuildDate = date('r');
+
+        $this->assertFalse($mp->isCached());
+        $mp->generate();
+        clearstatcache();
+        $this->assertTrue($mp->isCached());
+
+        $this->assertEquals($lastBuildDate, $mp->getLastBuildDate());
+        unset($mp); // release lock, in sub tests
+
+        age_dir_by('.', 3600);
+
+        $mp2 = $this->newPodcast();
+        $this->assertTrue($mp2->isCached());
+        $mp2->generate();
+        clearstatcache();
+        $this->assertTrue($mp2->isCached());
+
+        $this->assertEquals($lastBuildDate, $mp2->getLastBuildDate());
     }
 
     public function tearDown(): void
