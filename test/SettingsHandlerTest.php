c28d32465c58aaf24d92caed5643c688b3df8c8e
Ben XO <75862+ben-xo@users.noreply.github.com>
2021-03-18 22:05:46 +0000

    Add more defaults / bootstrap tests

--- test/SettingsHandlerTest.php
+++ test/SettingsHandlerTest.php
@@ -2,9 +2,6 @@
 
 use PHPUnit\Framework\TestCase;
 
-/**
- * @runTestsInSeparateProcesses
- */
 class SettingsHandlerTest extends TestCase
 {
     static $DEFINE_LIST = array(
@@ -46,6 +43,10 @@ class SettingsHandlerTest extends TestCase
         'MIN_FILE_AGE',
     );
     
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
     public function test_default_defines_set()
     {       
         foreach(self::$DEFINE_LIST as $define_name)
@@ -53,20 +54,32 @@ class SettingsHandlerTest extends TestCase
             $this->assertFalse(defined($define_name));
         }
         
-        SettingsHandler::bootstrap(array(), array(), array());
+        SettingsHandler::bootstrap(
+            /* $SERVER */ array(),
+            /* $GET */ array(),
+            /* $argv */ array()
+        );
         SettingsHandler::defaults();
         
         foreach(self::$DEFINE_LIST as $define_name)
         {
             $this->assertTrue(defined($define_name));
         }
+        
+        // should not be defined as $argv was empty
+        $this->assertFalse(defined('CLI_ONLY'));
     }
 
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
     public function test_behaves_if_anything_is_already_defined()
     {
         foreach(self::$DEFINE_LIST as $define_name)
         {
-            define($define_name, $define_name);
+            if($define_name != 'DIR2CAST_BASE') // always defined by bootstrap()
+                define($define_name, $define_name);
         }
         
         SettingsHandler::bootstrap(array(), array(), array());
@@ -74,8 +87,21 @@ class SettingsHandlerTest extends TestCase
         
         foreach(self::$DEFINE_LIST as $define_name)
         {
-            $this->assertEquals($define_name, constant($define_name));
+            if($define_name != 'DIR2CAST_BASE')
+                $this->assertEquals($define_name, constant($define_name));
         }
+    }
+    
+    /**
+     * @preserveGlobalState disabled
+     * @preserveGlobalState disabled
+     */
+    public function test_defines_CLI_ONLY_if_argv0()
+    {
+        // opposite test is in test_default_defines_set() above
+        $this->assertFalse(defined('CLI_ONLY'));
+        SettingsHandler::bootstrap(array(), array(), array('dir2cast.php'));
+        $this->assertTrue(defined('CLI_ONLY'));
     }
     
 }
