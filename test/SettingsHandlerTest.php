e6799953ce8aa29c4125be5ffc03d53c9e94eb50
Ben XO <75862+ben-xo@users.noreply.github.com>
2021-03-18 21:27:53 +0000

    Test that bootstrap() / default() sets some defines.

---
+++ test/SettingsHandlerTest.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class SettingsHandlerTest extends TestCase
+{
+    static $DEFINE_LIST = array(
+        // from boostrap
+        'DIR2CAST_BASE',
+        'MIN_CACHE_TIME',
+        'FORCE_PASSWORD',
+        'TMP_DIR',
+        'MP3_BASE',
+        'MP3_DIR',
+        
+        // from defaults
+        'MP3_URL',
+        'TITLE',
+        'LINK',
+        'RSS_LINK',
+        'DESCRIPTION',
+        'ATOM_TYPE',
+        'LANGUAGE',
+        'COPYRIGHT',
+        'TTL',
+        'ITEM_COUNT',
+        'ITUNES_SUBTITLE',
+        'ITUNES_SUMMARY',
+        'IMAGE',
+        'ITUNES_IMAGE',
+        'ITUNES_OWNER_NAME',
+        'ITUNES_OWNER_EMAIL',
+        'WEBMASTER',
+        'ITUNES_AUTHOR',
+        'ITUNES_CATEGORIES',
+        'ITUNES_EXPLICIT',
+        'LONG_TITLES',
+        'ITUNES_SUBTITLE_SUFFIX',
+        'DESCRIPTION_SOURCE',
+        'RECURSIVE_DIRECTORY_ITERATOR',
+        'AUTO_SAVE_COVER_ART',
+        'DONT_UNCACHE_IF_OUTPUT_FILE',
+        'MIN_FILE_AGE',
+    );
+    
+    public function test_default_defines_set()
+    {       
+        foreach(self::$DEFINE_LIST as $define_name)
+        {
+            $this->assertFalse(defined($define_name));
+        }
+        
+        SettingsHandler::bootstrap(array(), array(), array());
+        SettingsHandler::defaults();
+        
+        foreach(self::$DEFINE_LIST as $define_name)
+        {
+            $this->assertTrue(defined($define_name));
+        }
+    }
+    
+}
